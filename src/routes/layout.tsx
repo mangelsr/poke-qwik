03fb70b0c1cbde179bfea1aa957cbe9489977b87
Miguel Sanchez <mangelsr25@gmail.com>
2023-05-14 22:37:48 -0500

    [UPD] Project structure and components clean

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,26 +1,16 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import Navbar from "~/components/shared/navbar/navbar";
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import styles from "./styles.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
-      <main>
+      <Navbar />
+      <main class="flex flex-col items-center justify-center">
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
