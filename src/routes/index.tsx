5f544ccf0c9a180ce7b0265328a0126755140d4f
Miguel Sanchez <mangelsr25@gmail.com>
2023-05-14 22:51:44 -0500

    [UPD] Initial page structure and style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal(1);
+
   return (
     <>
-      <span class="text-5xl">Hello world</span>
+      <span class="text-2xl">Simple search</span>
+      <span class="text-9xl">{pokemonId}</span>
+      {/* TODO: Add pokemon image */}
+      <div class="mt-2">
+        <button class="btn btn-primary mr-2">Previous</button>
+        <button class="btn btn-primary">Next</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "PokeQwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Firsr qwik app",
     },
   ],
 };
