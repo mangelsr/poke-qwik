1515c05cb9820fa69c142d501f7eea536f25e359
Miguel Sanchez <mangelsr25@gmail.com>
2023-05-15 11:46:51 -0500

    [ADD] Show message while loading the sprite

--- src/components/pokemon/pokemon-image.tsx
+++ src/components/pokemon/pokemon-image.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 
 interface Props {
   id: number;
@@ -8,15 +8,35 @@ interface Props {
 
 export const PokemonImage = component$(
   ({ id, size = 200, backImage = false }: Props) => {
+    const isImageLoading = useSignal(false);
+
+    useTask$(({ track }) => {
+      track(() => id);
+      isImageLoading.value = false;
+    });
+
     return (
-      <img
-        width={size}
-        height={size}
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-          backImage ? "back" : ""
-        }/${id}.png`}
-        alt="Pokemon Sprite"
-      />
+      <div
+        class="flex items-center justify-center"
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+      >
+        {!isImageLoading.value && <span>Loading...</span>}
+        <img
+          width={size}
+          height={size}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            backImage ? "back" : ""
+          }/${id}.png`}
+          alt="Pokemon Sprite"
+          class={{
+            hidden: !isImageLoading.value,
+          }}
+          onLoad$={() => (isImageLoading.value = true)}
+        />
+      </div>
     );
   }
 );
