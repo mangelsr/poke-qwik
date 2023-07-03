e68221d197ebcc3b046f1d2bd0b73bb5fc2637d1
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-03 17:51:09 -0500

    [ADD] Base structure for client side list render

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,8 +1,33 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { DocumentHead, Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <div>Hello Client</div>;
+  return (
+    <>
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span class="">Current page: </span>
+        <span class="">Is loading:</span>
+      </div>
+
+      <div class="mt-10">
+        <button class="btn btn-primary mr-2">Previous</button>
+        <button class="btn btn-primary">Next</button>
+      </div>
+
+      <div class="grid grid-cols-6 mt-5">
+        {/* {pokemons.value.map((pokemon) => (
+          <div
+            key={pokemon.name}
+            class="m-5 flex flex-col justify-center items-center"
+          >
+            <PokemonImage id={pokemon.id} />
+            <span class="capitalize">{pokemon.name}</span>
+          </div>
+        ))} */}
+      </div>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
