e479779863794341f5a1f337baed0c2c1dbbeb10
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-04 12:20:27 -0500

    [UPD] Pokemon component to use custom game hook

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,8 +1,8 @@
-import { component$, useContext } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 
-import { PokemonGameContext } from "~/context/pokemon/pokemon-game.context";
 import { PokemonImage } from "~/components/pokemon/pokemon-image";
+import { usePokemonGame } from "~/hooks/use-pokemon-game";
 
 export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
   const id = Number(params.id);
@@ -13,15 +13,30 @@ export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
 });
 
 export default component$(() => {
-  const pokemonGame = useContext(PokemonGameContext);
+  const {
+    pokemonId,
+    showBackImage,
+    isPokemonVisible,
+    toggleFrontBack,
+    toggleVisible,
+  } = usePokemonGame();
+
   return (
     <>
-      <span class="text-5xl">Pokemon: {pokemonGame.pokemonId}</span>
+      <span class="text-5xl">Pokemon: {pokemonId.value}</span>
       <PokemonImage
-        id={pokemonGame.pokemonId}
-        isVisible={pokemonGame.isPokemonVisible}
-        backImage={pokemonGame.showBackImage}
+        id={pokemonId.value}
+        isVisible={isPokemonVisible.value}
+        backImage={showBackImage.value}
       />
+      <div class="mt-2">
+        <button onClick$={toggleFrontBack} class="btn btn-primary">
+          Flip
+        </button>
+        <button onClick$={toggleVisible} class="btn btn-primary ml-2">
+          {isPokemonVisible.value ? "Hide" : "Reveal"}
+        </button>
+      </div>
     </>
   );
 });
