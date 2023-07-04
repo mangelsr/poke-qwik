3bbf36fd7ebb955529997f77eead9a1fe0d6ff30
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-03 19:41:54 -0500

    [ADD] Initialize PokemonGameContext Provider

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,30 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import Navbar from "~/components/shared/navbar/navbar";
+import {
+  component$,
+  Slot,
+  useContextProvider,
+  useStore,
+  useStyles$,
+} from "@builder.io/qwik";
 
+import {
+  PokemonGameContext,
+  type PokemonGameState,
+} from "../context/pokemon/pokemon-game.context";
+
+import Navbar from "~/components/shared/navbar/navbar";
 import styles from "./styles.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
+
+  const pokemonGame = useStore<PokemonGameState>({
+    pokemonId: 4,
+    showBackImage: true,
+    isPokemonVisible: false,
+  });
+
+  useContextProvider(PokemonGameContext, pokemonGame);
+
   return (
     <>
       <Navbar />
