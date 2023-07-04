ab9e446fc7628144504332d8f7126b9764ac90a9
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-04 12:11:21 -0500

    [UPD] GameComponent to use custom hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,59 +1,49 @@
-import { $, component$, useContext } from "@builder.io/qwik";
+import { $, component$ } from "@builder.io/qwik";
 import { type DocumentHead, useNavigate } from "@builder.io/qwik-city";
 
 import { PokemonImage } from "~/components/pokemon/pokemon-image";
-import { PokemonGameContext } from "~/context";
+import { usePokemonGame } from "~/hooks/use-pokemon-game";
 
 export default component$(() => {
   const nav = useNavigate();
 
-  const pokemonGame = useContext(PokemonGameContext);
-
-  const changePokemonId = $((value: number) => {
-    if (pokemonGame.pokemonId + value <= 0) return;
-    pokemonGame.pokemonId += value;
-  });
+  const {
+    pokemonId,
+    showBackImage,
+    isPokemonVisible,
+    nextPokemon,
+    prevPokemon,
+    toggleFrontBack,
+    toggleVisible,
+  } = usePokemonGame();
 
   const goToPokemon = $(() => {
-    nav(`/pokemon/${pokemonGame.pokemonId}/`);
+    nav(`/pokemon/${pokemonId.value}/`);
   });
 
   return (
     <>
       <span class="text-2xl">Simple search</span>
-      <span class="text-9xl">{pokemonGame.pokemonId}</span>
+      <span class="text-9xl">{pokemonId.value}</span>
       <div onClick$={goToPokemon}>
         <PokemonImage
-          id={pokemonGame.pokemonId}
-          backImage={pokemonGame.showBackImage}
-          isVisible={pokemonGame.isPokemonVisible}
+          id={pokemonId.value}
+          backImage={showBackImage.value}
+          isVisible={isPokemonVisible.value}
         />
       </div>
       <div class="mt-2">
-        <button
-          onClick$={() => changePokemonId(-1)}
-          class="btn btn-primary mr-2"
-        >
+        <button onClick$={prevPokemon} class="btn btn-primary mr-2">
           Previous
         </button>
-        <button onClick$={() => changePokemonId(+1)} class="btn btn-primary">
+        <button onClick$={nextPokemon} class="btn btn-primary">
           Next
         </button>
-        <button
-          onClick$={() =>
-            (pokemonGame.showBackImage = !pokemonGame.showBackImage)
-          }
-          class="btn btn-primary ml-2"
-        >
+        <button onClick$={toggleFrontBack} class="btn btn-primary ml-2">
           Flip
         </button>
-        <button
-          onClick$={() =>
-            (pokemonGame.isPokemonVisible = !pokemonGame.isPokemonVisible)
-          }
-          class="btn btn-primary ml-2"
-        >
-          {pokemonGame.isPokemonVisible ? "Hide" : "Reveal"}
+        <button onClick$={toggleVisible} class="btn btn-primary ml-2">
+          {isPokemonVisible.value ? "Hide" : "Reveal"}
         </button>
       </div>
     </>
