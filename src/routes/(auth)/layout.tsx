5dffa76c67f4a132186af68d26036e1a87da9280
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-11 23:05:30 -0500

    [ADD] Styles and base Login component

--- src/routes/(auth)/layout.tsx
+++ src/routes/(auth)/layout.tsx
@@ -1,10 +1,23 @@
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "./auth-layout.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(styles);
   return (
-    <>
-      <h2>Auth Layout</h2>
-      <Slot />
-    </>
+    <div class="login-main">
+      <div class="login-bg">
+        <div class="login-bg-gradient"></div>
+        <div class="login-white-bg">
+          <div class="max-w-md mx-auto">
+            <div>
+              <h1 class="login-title">Login Form</h1>
+            </div>
+            <div class="login-content">
+              <Slot />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 });
--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,9 +1,32 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+
+import styles from "./login.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(styles);
+
   return (
-    <>
-      <h3>Login Form</h3>
-    </>
+    <form class="login-form">
+      <div class="relative">
+        <input name="email" type="text" placeholder="Email address" />
+        <label for="email">Email Address</label>
+      </div>
+      <div class="relative">
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <label for="password">Password</label>
+      </div>
+      <div class="relative">
+        <button>Ingresar</button>
+      </div>
+
+      {/* <code>
+                { JSON.stringify( formState, undefined , 2 ) }
+            </code> */}
+    </form>
   );
 });
