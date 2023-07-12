a9fd5b8af5619167f76d20bb6bd3e648bcba2523
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-11 23:21:39 -0500

    [ADD] Base features for form handling

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,32 +1,52 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStore, useStylesScoped$, $ } from "@builder.io/qwik";
 
 import styles from "./login.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const formState = useStore({
+    email: "default@email.com",
+    password: "123456",
+  });
+
+  const onSubmit = $(() => {
+    const { email, password } = formState;
+    console.log({ email, password });
+  });
+
   return (
-    <form class="login-form">
+    <form onSubmit$={onSubmit} class="login-form" preventdefault:submit>
       <div class="relative">
-        <input name="email" type="text" placeholder="Email address" />
+        <input
+          value={formState.email}
+          onInput$={(event) => {
+            formState.email = (event.target as HTMLInputElement).value;
+          }}
+          name="email"
+          type="text"
+          placeholder="Email address"
+        />
         <label for="email">Email Address</label>
       </div>
       <div class="relative">
         <input
-          id="password"
+          value={formState.password}
+          onInput$={(event) => {
+            formState.password = (event.target as HTMLInputElement).value;
+          }}
           name="password"
           type="password"
           placeholder="Password"
         />
         <label for="password">Password</label>
       </div>
+
       <div class="relative">
-        <button>Ingresar</button>
+        <button type="submit">Ingresar</button>
       </div>
 
-      {/* <code>
-                { JSON.stringify( formState, undefined , 2 ) }
-            </code> */}
+      <code>{JSON.stringify(formState, undefined, 2)}</code>
     </form>
   );
 });
