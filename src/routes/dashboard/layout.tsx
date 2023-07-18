91e874be810960b62bf62942b1a17cc010729c52
Miguel Sanchez <mangelsr25@gmail.com>
2023-07-17 22:33:44 -0500

    [ADD] Route protection via cookie validation

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,5 +1,18 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
 import Navbar from "~/components/shared/navbar/navbar";
+
+export const useCheckAuthCookie = routeLoader$(({ cookie, redirect }) => {
+  const jwtCookie = cookie.get("jwt");
+
+  // Do validation on server side here...
+  if (jwtCookie) {
+    console.log("Cookie value", jwtCookie);
+  } else {
+    redirect(302, "/login");
+  }
+});
 
 export default component$(() => {
   return (
